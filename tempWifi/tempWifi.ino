2abdd5bd11aa491bb1c6686ed0c6c32a1f721749
Kacper Lukasik <k.lukasik@blunovation.com>
2022-03-16 23:51:09 +0100

    Add RGB Led, and send led info

--- tempWifi/tempWifi.ino
+++ tempWifi/tempWifi.ino
@@ -3,58 +3,110 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecure.h>
 
-#define DHTPIN 4    
-#define DHTTYPE DHT11 
+#define DHTPIN 4
+#define DHTTYPE DHT11
 
-#define API_KEY "API_KEY"
+#define SEND_LED_PIN 2
+#define R_LED_PIN 12
+#define G_LED_PIN 13
+#define B_LED_PIN 15
+
+#define API_KEY "33LU8KGICUXUBCZK"
 #define API_URL "https://api.thingspeak.com/update"
 
-#define WIFI_SSID "SSID"
-#define WIFI_PASS "PASS"
-
+#define WIFI_SSID "WIFI_SSID"
+#define WIFI_PASS "WIFI_PASS"
 
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
+void setColor(int r, int g, int b)
+{
+  int rr = 255 - r;
+  int gg = 255 - g;
+  int bb = 255 - b;
+
+  analogWrite(R_LED_PIN, rr);
+  analogWrite(G_LED_PIN, gg);
+  analogWrite(B_LED_PIN, bb);
+}
+
+void setup()
+{
   Serial.begin(9600);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
   dht.begin();
-  WiFi.begin(WIFI_SSID,WIFI_PASS);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  pinMode(R_LED_PIN, OUTPUT);
+  pinMode(G_LED_PIN, OUTPUT);
+  pinMode(B_LED_PIN, OUTPUT);
+  pinMode(SEND_LED_PIN, OUTPUT);
+  digitalWrite(SEND_LED_PIN, HIGH);
+
+  setColor(255, 0, 0);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    setColor(0, 0, 0);
+    delay(300);
     Serial.println("Waiting for connection");
+    setColor(0, 150, 255);
+    delay(200);
   }
-  
+  setColor(0, 255, 0);
 }
 
-void sendToServer(float t, float h) {
-    WiFiClientSecure client;
-    client.setInsecure();
-    HTTPClient http;
+void sendToServer(float t, float h)
+{
+  WiFiClientSecure client;
+  HTTPClient http;
 
-    if( http.begin(client, String(API_URL) + "?api_key=" + String(API_KEY) + "&field1=" + h + "&field2=" + t)){
-      http.addHeader("Content-Type", "text/plain");
-      int resultCode = http.GET(); 
-      if(resultCode > 0){
-          Serial.println("Wysłano");
-      }
-      else{
-        Serial.println("Błąd wysyłania");
-      }
-      http.end();
+  client.setInsecure();
+  String url = String(API_URL) + "?api_key=" + String(API_KEY) + "&field1=" + h + "&field2=" + t;
+
+  if (http.begin(client, url))
+  {
+    http.addHeader("Content-Type", "text/plain");
+    int resultCode = http.GET();
+
+    if (resultCode > 0)
+    {
+      Serial.println("Wysłano - " + String(resultCode) + " | " + url);
     }
+    else
+    {
+      Serial.println("Błąd wysyłania");
+    }
+
+    http.end();
+  }
 }
 
-void loop() {
-  delay(2000);
+void setTemperatureColor(float t)
+{
+  setColor(0, 0, 0);
+  if (t <= 19.0)
+    setColor(0, 80, 252);
+  else if (t > 19.0 && t <= 23.0)
+    setColor(0, 255, 69);
+  else if (t > 23.0 && t <= 30.0)
+    setColor(235, 252, 3);
+  else if (t > 30.0)
+    setColor(252, 10, 0);
+}
 
+void loop()
+{
+  delay(2000);
   float h = dht.readHumidity();
   float t = dht.readTemperature();
 
-  if (isnan(h) || isnan(t)) {
+  if (isnan(h) || isnan(t))
+  {
     Serial.println(F("Nie można odczytać sensora!"));
     return;
   }
-
-  sendToServer(t,h);
+  digitalWrite(SEND_LED_PIN, LOW);
+  sendToServer(t, h);
+  digitalWrite(SEND_LED_PIN, HIGH);
+  // setTemperatureColor(t);
+  setColor(0, 0, 0);
 }
